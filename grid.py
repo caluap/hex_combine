dc8b1cb5595c72366d4fbb3fa832b10705c7b712
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-01-07 17:18:05 -0200

    random tile placement

--- grid.py
+++ grid.py
@@ -1,17 +1,28 @@
+import random
 tile_size = imageSize('tile.pdf')
+
+def randomFilename():
+    s = ['_','_','_','_','_','_']
+    l = ['a','b','c','d','e','f']
+    for i in range(6):
+        if random.choice([True, False]):
+            s[i] = l[i]
+    return ''.join(s)
 
 def pos(ix,iy):
     adj_x = int((iy % 2) * tile_size[0]/2)
     adj_y = int(tile_size[1] * 0.75)
-    x = ix*tile_size[0]+adj_x
-    y = iy*adj_y
+    
+    # I subtract half a tile_size so they go till they bleed the page
+    x = ix*tile_size[0]+adj_x - tile_size[0]//2
+    y = iy*adj_y - tile_size[1]//2
     return x, y
 
 def main():
-    size('A3')
+    size('A2')
 
-    w = round(width()/tile_size[0])
-    h = round(height()/(tile_size[1]*0.75))
+    w = round(width()/tile_size[0]) + 1
+    h = round(height()/(tile_size[1]*0.75)) + 1
 
     print(f'{w,h}')
 
@@ -19,7 +30,12 @@ def main():
     for iy in range(h):
         for ix in range(w):
             x,y = pos(ix,iy)
+
+            rf = './combined/' + randomFilename() + '.pdf'
+            image(rf, (x,y))
+            
             image('tile.pdf', (x, y))
+            
         
     
 main()
