32e305f658212a2e4c14549efa8d67eb3a11f6c4
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-01-07 17:46:25 -0200

    tiles as objects (will be useful soon)

--- grid.py
+++ grid.py
@@ -1,5 +1,22 @@
 import random
+from pprint import pprint
 tile_size = imageSize('tile.pdf')
+
+
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.filename = randomFilename()
+        self.pos_x, self.pos_y = pos(x,y)
+        
+    def print(self, print_tile_border = False):
+        rf = './combined/' + self.filename + '.pdf'
+        image(rf, (self.pos_x, self.pos_y))
+        if print_tile_border:
+            image('tile.pdf', (self.pos_x, self.pos_y))
+
+
 
 def randomFilename():
     s = ['_','_','_','_','_','_']
@@ -25,16 +42,14 @@ def main():
     h = round(height()/(tile_size[1]*0.75)) + 1
 
     print(f'{w,h}')
-
+    
+    tiles = []
 
     for iy in range(h):
+        tiles.append([])
         for ix in range(w):
-            x,y = pos(ix,iy)
-
-            rf = './combined/' + randomFilename() + '.pdf'
-            image(rf, (x,y))
-            
-            image('tile.pdf', (x, y))
+            tiles[iy].append(Tile(ix,iy))
+            tiles[iy][ix].print(True)
             
         
     
