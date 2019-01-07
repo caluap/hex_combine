7c61fec25cc91011b8c3ba6f2409f0dca4a05b07
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-01-07 17:10:46 -0200

    cleaning up

--- grid.py
+++ grid.py
@@ -1,19 +1,25 @@
-
-size('A3')
-
 tile_size = imageSize('tile.pdf')
 
-w = round(width()/tile_size[0])
-h = round(height()/(tile_size[1]*0.75))
-
-print(f'{w,h}')
-
-
-for iy in range(h):
+def pos(ix,iy):
     adj_x = int((iy % 2) * tile_size[0]/2)
     adj_y = int(tile_size[1] * 0.75)
-    for ix in range(w):
-        x = ix*tile_size[0]+adj_x
-        y = iy*adj_y
-        image('tile.pdf', (x, y))
-        # print(f'{x},{y}')
+    x = ix*tile_size[0]+adj_x
+    y = iy*adj_y
+    return x, y
+
+def main():
+    size('A3')
+
+    w = round(width()/tile_size[0])
+    h = round(height()/(tile_size[1]*0.75))
+
+    print(f'{w,h}')
+
+
+    for iy in range(h):
+        for ix in range(w):
+            x,y = pos(ix,iy)
+            image('tile.pdf', (x, y))
+        
+    
+main()
